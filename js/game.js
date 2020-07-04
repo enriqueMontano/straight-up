e6b272743609277782be1dc2b360001bddbf1022
enriqueMontano <montanoenrique@outlook.com>
2020-07-04 10:39:42 +0200

    Refactor clear canvas functions for reusability

--- js/game.js
+++ js/game.js
@@ -9,135 +9,127 @@ const Game = {
   score: 0,
   life: 5,
 
-  playerKeys: {
-    arrowUp: false,
-    arrowRight: false,
-    arrowDown: false,
-    arrowLeft: false,
-    space: false,
-  },
-
   player: undefined,
   enemies: [],
   explosions: [],
   firstPower: [],
 
   init: function () {
-    this.canvas = document.getElementById('canvas')
-    this.ctx = this.canvas.getContext('2d')
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
-    this.width = 512
-    this.height = 544
-    this.canvas.width = this.width
-    this.canvas.height = this.height
+    this.width = 512;
+    this.height = 544;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
 
-    this.start()
+    this.start();
   },
 
   start: function () {
-    this.reset()
-    this.musicGame.play()
+    this.reset();
+    this.musicGame.play();
     this.interval = setInterval(() => {
-      this.framesCounter++
+      this.framesCounter++;
 
-      this.clear()
-      this.drawAll()
+      this.clearCanvas();
+      this.drawAll();
 
-      this.generateClouds()
-      this.generateEnemies()
-      this.generatePowerUps()
+      this.generateClouds();
+      this.generateEnemies();
+      this.generatePowerUps();
 
-      if (this.isPlayerCollision(this.firstPower)) this.life++
+      if (this.isPlayerCollision(this.firstPower)) this.life++;
       if (this.isPlayerCollision(this.enemies)) {
-        this.life--
+        this.life--;
         this.explosions.push(
-          new Explosion(this.ctx, this.player.width, this.player.height, this.player.x, this.player.y)
-        )
+          new Explosion(
+            this.ctx,
+            this.player.width,
+            this.player.height,
+            this.player.x,
+            this.player.y
+          )
+        );
       }
-      this.isCollision(this.player.bullets, this.enemies)
+      this.isCollision(this.player.bullets, this.enemies);
 
-      this.clearClouds()
-      this.clearEnemys()
-      this.clearPowerUps()
-      if (this.life < 1) this.gameOver()
-      if (this.framesCounter > 1000) this.framesCounter = 0
-    }, 1000 / this.fps)
+      this.clear(this.enemies);
+      this.clear(this.explosions);
+      this.clear(this.firstPower);
+
+      if (this.life < 1) this.gameOver();
+      if (this.framesCounter > 1000) this.framesCounter = 0;
+    }, 1000 / this.fps);
   },
 
   reset() {
-    this.life = 5
-    this.score = 0
-    Score.init(this.ctx, this.score)
-    Lifes.init(this.ctx, this.life)
+    this.life = 5;
+    this.score = 0;
+    Score.init(this.ctx, this.score);
+    Lifes.init(this.ctx, this.life);
 
-    this.desertBackground = new Background(this.ctx, 512, 544)
+    this.desertBackground = new Background(this.ctx, 512, 544);
 
-    this.player = new Player(
-      this.ctx,
-      32,
-      48,
-      './img/ship.png',
-      this.width / 2 - 8,
-      this.height - 50,
-      this.playerKeys,
-      this.width,
-      this.height
-    )
+    this.player = new Player(this.ctx, this.width, this.height);
 
-    this.sClouds = []
-    this.tClouds = []
+    this.sClouds = [];
+    this.tClouds = [];
 
-    this.musicGame = new Audio()
-    this.musicGame.src = './audio/abandoned-hopes.mp3'
+    this.musicGame = new Audio();
+    this.musicGame.src = './audio/abandoned-hopes.mp3';
 
-    this.endGame = new Audio()
-    this.endGame.src = './audio/player-death.mp3'
+    this.endGame = new Audio();
+    this.endGame.src = './audio/player-death.mp3';
   },
 
   generateClouds: function () {
     if (this.framesCounter % 150 === 0)
-      this.tClouds.push(new Clouds(this.ctx, 512, 206, './img/clouds-transparent.png', 0, 0, 6))
+      this.tClouds.push(
+        new Clouds(this.ctx, 512, 206, './img/clouds-transparent.png', 0, 0, 6)
+      );
 
-    if (this.framesCounter % 350 === 0) this.sClouds.push(new Clouds(this.ctx, 1024, 412, './img/clouds.png', 0, 0, 12))
+    if (this.framesCounter % 350 === 0)
+      this.sClouds.push(
+        new Clouds(this.ctx, 1024, 412, './img/clouds.png', 0, 0, 12)
+      );
   },
 
   generateEnemies() {
-    if (this.framesCounter % 50 === 0) this.enemies.push(new EnemyFactory(this.ctx, this.randomPosX(), 'easy'))
-    if (this.framesCounter % 100 === 0) this.enemies.push(new EnemyFactory(this.ctx, this.randomPosX(), 'normal'))
-    if (this.framesCounter % 200 === 0) this.enemies.push(new EnemyFactory(this.ctx, this.randomPosX(), 'hard'))
+    if (this.framesCounter % 50 === 0)
+      this.enemies.push(new EnemyFactory(this.ctx, this.randomPosX(), 'easy'));
+    if (this.framesCounter % 100 === 0)
+      this.enemies.push(
+        new EnemyFactory(this.ctx, this.randomPosX(), 'normal')
+      );
+    if (this.framesCounter % 200 === 0)
+      this.enemies.push(new EnemyFactory(this.ctx, this.randomPosX(), 'hard'));
   },
 
   generatePowerUps: function () {
-    if (this.framesCounter % 50 === 0) this.firstPower.push(new Powerup(this.ctx, this.randomPosX()))
+    if (this.framesCounter % 50 === 0)
+      this.firstPower.push(new Powerup(this.ctx, this.randomPosX()));
   },
 
   drawAll: function () {
-    this.desertBackground.draw()
-    this.tClouds.forEach((cloud) => cloud.draw())
-    this.firstPower.forEach((powerUp) => powerUp.draw(this.framesCounter))
-    this.player.draw(this.framesCounter)
-    this.enemies.forEach((enemy) => enemy.draw(this.framesCounter))
-    this.explosions.forEach((explosion) => explosion.draw(this.framesCounter))
-    this.sClouds.forEach((cloud) => cloud.draw())
-    Score.draw(this.score)
-    Lifes.draw(this.life)
+    this.desertBackground.draw();
+    this.tClouds.forEach((cloud) => cloud.draw());
+    this.firstPower.forEach((powerUp) => powerUp.draw(this.framesCounter));
+    this.player.draw(this.framesCounter);
+    this.enemies.forEach((enemy) => enemy.draw(this.framesCounter));
+    this.explosions.forEach((explosion) => explosion.draw(this.framesCounter));
+    this.sClouds.forEach((cloud) => cloud.draw());
+    Score.draw(this.score);
+    Lifes.draw(this.life);
   },
 
-  clear: function () {
-    this.ctx.clearRect(0, 0, this.width, this.height)
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
   },
 
-  clearClouds: function () {
-    this.sClouds = this.sClouds.filter((cloud) => cloud.posY - cloud.height <= this.height)
-    this.tClouds = this.tClouds.filter((cloud) => cloud.posY - cloud.height <= this.height)
-  },
-
-  clearEnemys() {
-    this.enemies = this.enemies.filter((enemy) => enemy.y - enemy.height <= this.height)
-  },
-
-  clearPowerUps: function () {
-    this.firstPower = this.firstPower.filter((powerUp) => powerUp.y <= this.height)
+  clear(arr) {
+    arr = arr.filter((elem) => elem.y - elem.height <= this.height);
+    console.log(arr);
   },
 
   isPlayerCollision(arr) {
@@ -148,11 +140,11 @@ const Game = {
         cv.y < this.player.y + this.player.height &&
         cv.height + cv.y > this.player.y
       ) {
-        arr.splice(cv, 1)
-        cv.sound.play()
-        return true
+        arr.splice(cv, 1);
+        cv.sound.play();
+        return true;
       }
-    })
+    });
   },
 
   isCollision(arr1, arr2) {
@@ -164,26 +156,28 @@ const Game = {
           cv1.y < cv2.y + cv2.height &&
           cv1.height + cv1.y > cv2.y
         ) {
-          arr1.splice(cv1, 1)
-          arr2.splice(cv2, 1)
-          cv2.sound.play()
-          this.score += 10
-          this.explosions.push(new Explosion(this.ctx, cv2.width, cv2.height, cv2.x, cv2.y))
+          arr1.splice(cv1, 1);
+          arr2.splice(cv2, 1);
+          cv2.sound.play();
+          this.score += 10;
+          this.explosions.push(
+            new Explosion(this.ctx, cv2.width, cv2.height, cv2.x, cv2.y)
+          );
         }
-      })
-    })
+      });
+    });
   },
 
   gameOver() {
     setTimeout(() => {
-      clearInterval(this.interval)
-      document.querySelector('#game-over').style.display = 'flex'
-    }, 500)
-    this.musicGame.pause()
-    this.endGame.play()
+      clearInterval(this.interval);
+      document.querySelector('#game-over').style.display = 'flex';
+    }, 500);
+    this.musicGame.pause();
+    this.endGame.play();
   },
 
   randomPosX() {
-    return Math.floor(Math.random() * (this.width - 0)) + 0
+    return Math.floor(Math.random() * (this.width - 0)) + 0;
   },
-}
+};
